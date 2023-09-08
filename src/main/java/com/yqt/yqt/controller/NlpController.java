7116baa008dc1808894c72d6812f3e8ec2793256
plusboy <no1plus88@gmail.com>
2023-09-08 10:30:56 +0800

    添加功能：文本比对

---
+++ src/main/java/com/yqt/yqt/controller/NlpController.java
@@ -0,0 +1,85 @@
+package com.yqt.yqt.controller;
+
+import com.yqt.yqt.entity.DiffMatchPatch;
+import com.yqt.yqt.entity.ExampleRequest;
+import com.yqt.yqt.util.ReturnUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("")
+public class NlpController {
+
+    /**
+     *
+     * 文本对比
+     */
+    @PostMapping("/text_comparison")
+    public Object textMatch(@RequestBody ExampleRequest request) {
+//        String text1="昨天与上海队的比赛，是深圳队老将周鹏CBA生涯的第684场比赛，他超越了前队友广东传奇名宿朱芳雨的683场纪录成为CBA历史出场数第一人！" ;
+//        String text2="上海队的比赛，老将周鹏CBA生涯的第684场比赛中，他超越前队友广东传奇名宿朱芳雨的冠军纪录成为CBA历史出场数第一人！";
+        String text1 = request.getText1();
+        String text2 = request.getText2();
+        DiffMatchPatch dmp = new DiffMatchPatch();
+        String htmlDiffString = dmp.getHtmlDiffString(text1,text2);
+//        String htmlDiffString = dmp.getHtmlDiffString(text1,text2);
+        System.out.println(htmlDiffString);
+
+        Document parse = Jsoup.parse(htmlDiffString);
+        ArrayList<Map<String,Object>> arrayList = new ArrayList<>();
+        Elements select = parse.select("span");
+        for (Element element : select) {
+            Map<String,Object> map = new HashMap<>();
+            map.put("diff_type","equal");
+            String[] titles = element.attr("title").split("=");
+            map.put("begin_pos",titles[1]);
+            map.put("end_pos",Integer.parseInt(titles[1])+(element.text().length()));
+            map.put("diff_txt",element.text());
+            arrayList.add(map);
+            //System.out.println(element.toString());
+            System.out.println("span;"+element.attr("title")+"        "+element.text());
+        }
+
+        Elements select_del = parse.select("del");
+        for (Element element : select_del) {
+            Map<String,Object> map = new HashMap<>();
+            map.put("diff_type","delete");
+            String[] titles = element.attr("title").split("=");
+            map.put("begin_pos",titles[1]);
+            map.put("end_pos",Integer.parseInt(titles[1])+element.text().length());
+            map.put("diff_txt",element.text());
+            arrayList.add(map);
+            //System.out.println(element.toString());
+            System.out.println("del"+element.attr("title")+"        "+element.text());
+        }
+
+
+        Elements select_ins = parse.select("ins");
+        for (Element element : select_ins) {
+            Map<String,Object> map = new HashMap<>();
+            map.put("diff_type","add");
+            String[] titles = element.attr("title").split("=");
+            map.put("begin_pos",titles[1]);
+            map.put("end_pos",Integer.parseInt(titles[1])+element.text().length());
+            map.put("diff_txt",element.text());
+            arrayList.add(map);
+            //System.out.println(element.toString());
+            System.out.println("ins:"+element.attr("title")+"        "+element.text());
+        }
+        arrayList.sort(Comparator.comparing((Map m) -> (new BigDecimal(m.get("begin_pos").toString()))));
+        Map<String,Object> result = new HashMap<>();
+        result.put("html",htmlDiffString);
+        result.put("data",arrayList);
+        return ReturnUtil.success("200","成功",result);
+    }
+
+}
---
+++ src/main/java/com/yqt/yqt/entity/ExampleRequest.java
@@ -0,0 +1,11 @@
+package com.yqt.yqt.entity;
+
+import lombok.Data;
+
+@Data
+public class ExampleRequest {
+    private String text1;
+    private String text2;
+
+    // getters and setters
+}
