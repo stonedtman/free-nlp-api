698cd13ea76bcd5c7e46ed5796c526c5829f6e21
plusboy <no1plus88@gmail.com>
2023-09-19 10:48:02 +0800

    添加功能：招投标抽取

--- src/main/java/com/yqt/yqt/controller/NlpController.java
+++ src/main/java/com/yqt/yqt/controller/NlpController.java
@@ -31,6 +31,9 @@ public class NlpController {
 
     @Value("${url.extractContract}")
     private String url_extractContract;
+
+    @Value("${url.extractBidding}")
+    private String url_extractBidding;
     /**
      *
      * 文本对比
@@ -197,6 +200,39 @@ public class NlpController {
         JSONArray jsonArray = JSONArray.parseArray(body);
         JSONObject returnObj = new JSONObject();
         returnObj.put("msg", "合同抽取成功");
+        returnObj.put("code", "200");
+        returnObj.put("result", jsonArray);
+        return returnObj;
+    }
+
+    /**
+     * 5.信息抽取-招投标抽取
+     */
+    @PostMapping("/extractBidding")
+    public Object extractBidding(@RequestBody Map<String, Object> param) {
+        RestTemplateUtil rtu = new RestTemplateUtil();
+        String text = "";
+        if (param == null || param.get("text") == null || String.valueOf(param.get("text")).length() < 1) {
+            return ReturnUtil.error("501", "传参有误 或 传参内容为空");
+        } else {
+            if (String.valueOf(param.get("text")).length() > 5000) {
+                text = String.valueOf(param.get("text")).substring(0, 5000);
+            } else {
+                text = String.valueOf(param.get("text"));
+            }
+        }
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("text", text);
+        //调用第三方接口
+//        String body = rtu.post("http://stonedt.com:8383/extractBidding",params);
+        String body = rtu.post(url_extractBidding, params);
+        //处理返回string
+        body = body.replace("\"[", "[").replace("]\"", "]").replace("\\", "");
+        //转jsonArr
+        JSONArray jsonArray = JSONArray.parseArray(body);
+        JSONObject returnObj = new JSONObject();
+        returnObj.put("msg", "招投标抽取成功");
         returnObj.put("code", "200");
         returnObj.put("result", jsonArray);
         return returnObj;
