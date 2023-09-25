34042ea163ccabd2b2d11838fccfcda0dc349417
plusboy <no1plus88@gmail.com>
2023-09-25 17:20:34 +0800

    添加新功能 信息抽取-观点抽取

--- src/main/java/com/yqt/yqt/controller/NlpController.java
+++ src/main/java/com/yqt/yqt/controller/NlpController.java
@@ -14,6 +14,7 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -40,9 +41,12 @@ public class NlpController {
 
     @Value("${url.extractResume}")
     private String url_extractResume;
+
+    @Value("${url.extractAppraise}")
+    private String url_extractAppraise;
     /**
      *
-     * 文本对比
+     * 1、文本对比
      */
     @PostMapping("/text_comparison")
     public Object textMatch(@RequestBody ExampleRequest request) {
@@ -52,9 +56,7 @@ public class NlpController {
         String text2 = request.getText2();
         DiffMatchPatch dmp = new DiffMatchPatch();
         String htmlDiffString = dmp.getHtmlDiffString(text1,text2);
-//        String htmlDiffString = dmp.getHtmlDiffString(text1,text2);
         System.out.println(htmlDiffString);
-
         Document parse = Jsoup.parse(htmlDiffString);
         ArrayList<Map<String,Object>> arrayList = new ArrayList<>();
         Elements select = parse.select("span");
@@ -104,7 +106,7 @@ public class NlpController {
     }
 
     /**
-     * 9.文本分类  通用模型
+     * 2.文本分类  通用模型
      */
     @PostMapping("/classify")
     public Object common_cate(@RequestBody Map<String, Object> param) {
@@ -128,7 +130,7 @@ public class NlpController {
     }
 
     /**
-     * 通用情感分析  sentiment
+     * 3.通用情感分析  sentiment
      */
     @PostMapping("/sentiment")
     public Object sentiment(@RequestBody Map<String, Object> param) {
@@ -178,7 +180,7 @@ public class NlpController {
     }
 
     /**
-     * 合同抽取：文本内容抽取
+     * 4.合同抽取：文本内容抽取
      * @param param
      * @return
      */
@@ -278,7 +280,7 @@ public class NlpController {
     }
 
     /**
-     * 4.信息抽取-简历抽取
+     * 7.信息抽取-简历抽取
      */
     @PostMapping("/extractResume")
     public Object extractResume(@RequestBody Map<String, Object> param) {
@@ -309,4 +311,36 @@ public class NlpController {
         returnObj.put("result", jsonArray);
         return returnObj;
     }
+
+    /**
+     * 8.信息抽取-观点抽取
+     */
+    @PostMapping("/extractAppraise")
+    public Object extractAppraise(@RequestBody Map<String, Object> param,
+                                  HttpServletRequest request) {
+        RestTemplateUtil rtu = new RestTemplateUtil();
+        String text = "";
+        if (param == null || param.get("text") == null || String.valueOf(param.get("text")).length() < 1) {
+            return ReturnUtil.error("501", "传参有误 或 传参内容为空");
+        } else {
+            if (String.valueOf(param.get("text")).length() > 5000) {
+                text = String.valueOf(param.get("text")).substring(0, 5000);
+            } else {
+                text = String.valueOf(param.get("text"));
+            }
+        }
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("text", text);
+        String body = rtu.post(url_extractAppraise, params);
+        //处理返回string
+        body = body.replace("\"[", "[").replace("]\"", "]").replace("\\", "");
+        //转jsonArr
+        JSONArray jsonArray = JSONArray.parseArray(body);
+        JSONObject returnObj = new JSONObject();
+        returnObj.put("msg", "观点抽取成功");
+        returnObj.put("code", "200");
+        returnObj.put("result", jsonArray);
+        return returnObj;
+    }
+
 }
